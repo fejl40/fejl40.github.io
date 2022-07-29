3e8f6565d85cdbe0ddb3940ef3449597651c56bf
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-07-29 19:41:00 +0200

    Added dist to git for hosting and changed favicon.

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import { Router, Route, Link } from "svelte-navigator";
-	
-
+	import { Router, Route } from "svelte-navigator";
 	import HomePage from "./pages/HomePage.svelte"
 	import MapPage from "./pages/MapPage.svelte"
-	
 </script>
 
 <Router>
-	<main>
-		<Route path="/" component={HomePage}/>
-		<Route path="/:map" component={MapPage} />
-	</main>
+	<Route path="/" component={HomePage}/>
+	<Route path="/:map" component={MapPage} />
 </Router>
--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
     import manifest_json from "../map-manifest.json"
     let mapNames = Object.keys(manifest_json);
+    
+    let mapBackgroundImages: {[mapName:string]: string} = {};
+    for (let i = 0; i < mapNames.length; i++) {
+        const n = mapNames[i];
+        if (n === "ancient") {
+            mapBackgroundImages[n] = `${n}.jpeg`;
+            continue;
+        }
+        mapBackgroundImages[n] = `${n}.jpg`;
+    }
 </script>
 
-<div>
-    <ul>
-        {#each mapNames as map}
-            <li>
-                <a href={`/${map}`}>{map}</a>
-            </li>
-        {/each}
-    </ul>
+<div class="grid grid-flow-col grid-rows-3 lg:grid-rows-2 container px-1 mx-auto mt-24">
+    {#each mapNames as map}
+        <div 
+            style={`background-image: url("/backgrounds/${mapBackgroundImages[map]}");`}
+            alt={map}
+            class="map-button-size mx-auto rounded-xl shadow-inner hover:opacity-50 mt-1"
+        >
+            <a href={`/${map}`}>
+                <div class="w-full h-full"></div>
+            </a>
+        </div>
+    {/each}
 </div>
   
+<style>
+    .map-button-size {
+        width: 504px;
+        height: 300px;
+    }
+</style>
